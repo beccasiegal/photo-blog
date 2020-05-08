cb4ea6d7358d6548ba59e573e5481a802d529fb2
Rebecca Siegal <rebecca@Rebeccas-MacBook-Air.local>
2020-05-08 10:18:17 -0400

    Updated project

--- src/App/App.js
+++ src/App/App.js
@@ -1,26 +1,137 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Route, Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Articles from '../Articles/Articles'
+import Comments from '../Comments/Comments'
+import Pictures from '../Pictures/Pictures'
+import AddArticle from '../AddArticle/AddArticle'
+import AddComment from '../AddComment/AddComment'
+import AddPicture from '../AddPicture/AddPicture'
+import dummyStore from '../dummy-store'
+import config from '../config'
+import {findArticle, findComment, getCommentsForArticle} from '../articles-helper'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = {
+   articles: [],
+    comments: [],
+    pictures: [],
+  };
+
+  componentDidMount() {
+    // fake date loading from API call
+    setTimeout(() => this.setState(dummyStore), 600)
+    
+  }
+  
+
+  renderNavRoutes() {
+    const { articles, comments, pictures } = this.state
+    return (
+      <div>
+        {['/', '/articles/articlesid'].map(path =>
+          <Route
+            exact
+            key={path}
+            path={path}
+            render={routeProps =>
+              <Articles
+                articles={articles}
+                comments={comments}
+                pictures={pictures}
+                {...routeProps}
+              />
+            }
+          />
+        )}
+        <Route
+          path='/comments/:articleId'
+          render={routeProps => {
+            const { articleId } = routeProps.match.params
+            const comment = findComment(comment, articleId) || {}
+            const article = findArticle(article, articleId)
+            return (
+              <Articles
+                {...routeProps}
+                article={article}
+              />
+            )
+          }}
+        />
+        <Route
+          path='/add-article'
+          component={Articles}
+        />
+        <Route
+          path='/add-comment'
+          component={Comments}
+        />
+      </div>
+    )
+  }
+
+  renderMainRoutes() {
+    const { pictures, articles } = this.state
+    return (
+      <div>
+        {['/', '/articles/:articleId'].map(path =>
+          <Route
+            exact
+            key={path}
+            path={path}
+            render={routeProps => {
+              const { articleId } = routeProps.match.params
+              const  comments = (comments, articleId)
+              return (
+                <Names
+                  {...routeProps}
+                  comments={comments}
+                />
+              )
+            }}
+          />
+        )}
+        
+      
+        <Route
+          path='/add-article'
+          component={AddArticle}
+        />
+        <Route
+          path='/add-picture'
+          render={routeProps => {
+            return (
+              <AddPicture
+                {...routeProps}
+                articles={article}
+              />
+            )
+          }}
+        />
+      </div>
+    )
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <nav className='App__nav'>
+          {this.renderNavRoutes()}
+        </nav>
+        <header className='App__header'>
+          <h1>
+            <Link to='/'>The Momentary Pause</Link>
+            {' '}
+          </h1>
+        </header>
+        <main className='App__main'>
+          {this.renderMainRoutes()}
+        </main>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
